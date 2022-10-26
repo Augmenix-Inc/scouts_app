ec741a3f11a605b51e6de41fb77d43162708dc0c
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-10-26 22:55:35 +0800

    Changes

--- lib/signin.dart
+++ lib/signin.dart
@@ -12,64 +12,76 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 150.0,
-            width: 190.0,
-            padding: const EdgeInsets.only(top: 40),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(200),
-            ),
-            child: const Center(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 50),
               child: Text(
-                  "Image Goes here"), // Image.asset('asset/images/flutter-logo.png'),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'test@test.com'),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'MyVerySafePassword'),
-            ),
-          ),
-          Row(
-            children: [
-              const Spacer(),
-              TextButton(
-                onPressed: () {},
-                child: Text('Sign Up'),
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const Spacer(),
-              TextButton(
-                onPressed: () {},
-                child: Text('Forgot Password'),
-              ),
-              const Spacer()
-            ],
-          ),
-          ElevatedButton(
-            child: Padding(
-              child: Text('Sign In'),
-              padding: EdgeInsets.only(left: 25, right: 25),
             ),
-            //print hi
-            onPressed: () {},
-          ),
-        ],
+            Container(
+              height: 150.0,
+              width: 190.0,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(200),
+              ),
+              child: const Center(
+                child: Text(
+                    "Image Goes here"), // Image.asset('asset/images/flutter-logo.png'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'test@test.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'MyVerySafePassword'),
+              ),
+            ),
+            Row(
+              children: [
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Sign Up'),
+                ),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot Password'),
+                ),
+                const Spacer()
+              ],
+            ),
+            ElevatedButton(
+              child: Padding(
+                child: Text('Sign In'),
+                padding: EdgeInsets.only(left: 25, right: 25),
+              ),
+              //print hi
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
